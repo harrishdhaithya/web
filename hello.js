69f58c353a66d8e8fd5518419490c4eabaf1e850
harrishdhaithya <67573978+harrishdhaithya@users.noreply.github.com>
2020-07-03 12:00:05 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("button").click(function functionName() {
+    let v1=$("#val1").val();
+    let v2=$("#val2").val();
+    let sum=v1+v2;
+    alert('The sum is '+ sum)
+  });
+});
